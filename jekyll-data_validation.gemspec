b389f05b773a1ff489e0602805b0bf9b521ac0d1
Greg Scott <i@gregoryjscott.com>
2015-10-09 16:00:56 -0500

    Validates unc dates

--- jekyll-data_validation.gemspec
+++ jekyll-data_validation.gemspec
@@ -13,6 +13,8 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ['lib']
 
+  spec.add_runtime_dependency 'json-schema', '~> 2.5'
+
   spec.add_development_dependency 'bundler', '~> 1.7'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'jekyll', '~> 2.5'
--- lib/jekyll/data_validation.rb
+++ lib/jekyll/data_validation.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'json-schema'
 
 module Jekyll
   class DataValidation
@@ -12,28 +13,25 @@ module Jekyll
     end
 
     def validate_pages
+      schema = {
+        # 'required' => ['permalink'],
+        'properties' => {
+          'unc' => {
+            # 'type' => 'string',
+            'format' => 'date-time'
+          }
+        }
+      }
+
       @site.pages.each do |page|
-        unless page.permalink.nil?
-          page_dir = File.dirname(page.permalink)
-          page_name = File.basename(page.permalink, ".*")
-          sub_dir = 'api'
-          sub_dir = File.join(sub_dir, page_dir) unless page_dir == '.'
-          sub_dir = File.join(sub_dir, page_name) unless page_name == 'index'
-
-          path = @site.source
-          path = File.join(path, sub_dir)
-          FileUtils.mkdir_p(path) unless Dir.exists?(path)
-
-          data = page.data
-          data['source-file'] = page.path
-
-          name = 'data.json'
-          File.write(File.join(path, name), JSON.pretty_generate(data))
-          json_file = Jekyll::StaticFile.new(@site, @site.source, sub_dir, name)
-          @site.static_files << json_file
+        data = page.data
+        begin
+          JSON::Validator.validate!(schema, data)
+        rescue JSON::Schema::ValidationError => e
+          puts "File: #{page.path} failed validation."
+          puts "Error: #{e.message}"
         end
       end
     end
-
   end
 end
