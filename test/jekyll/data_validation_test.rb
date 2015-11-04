699138aa06badf12a18b514569ad83e410030276
Greg Scott <i@gregoryjscott.com>
2015-11-04 13:46:03 -0700

    Group tests by format

--- test/jekyll/data_validation_test.rb
+++ test/jekyll/data_validation_test.rb
@@ -13,58 +13,64 @@ describe Jekyll::DataValidation do
     @errors = validator.validate
   end
 
-  it 'allows quoted short date' do
-    assert_no_errors 'short_date'
+  describe 'short date' do
+    it 'allows quoted short date' do
+      assert_no_errors 'short_date'
+    end
+
+    it 'creates error for invalid short date' do
+      assert_has_errors 'short_date_bad_month'
+    end
+
+    it 'creates error for short date without quotes' do
+      assert_has_errors 'short_date_no_quotes'
+    end
+
+    it 'creates error if short date has wrong format' do
+      assert_has_errors 'short_date_with_wrong_short_time_format'
+      assert_has_errors 'short_date_with_wrong_short_date_time_format'
+    end
   end
 
-  it 'allows quoted short time' do
-    assert_no_errors 'short_time'
+  describe 'short time' do
+    it 'allows quoted short time' do
+      assert_no_errors 'short_time'
+    end
+
+    it 'creates error for invalid short time' do
+      assert_has_errors 'short_time_bad_hours'
+    end
+
+    it 'creates error for short time without quotes' do
+      assert_has_errors 'short_time_no_quotes'
+    end
+
+    it 'creates error if short time has wrong format' do
+      assert_has_errors 'short_time_with_wrong_short_date_format'
+      assert_has_errors 'short_time_with_wrong_short_date_time_format'
+    end
   end
 
-  it 'allows quoted short date time' do
-    assert_no_errors 'short_date_time'
-  end
+  describe 'short date time' do
+    it 'allows quoted short date time' do
+      assert_no_errors 'short_date_time'
+    end
 
-  it 'creates error for invalid short date' do
-    assert_has_errors 'short_date_bad_month'
-  end
+    it 'creates error for invalid short date time' do
+      assert_has_errors 'short_date_time_bad_month'
+      assert_has_errors 'short_date_time_bad_hours'
+    end
 
-  it 'creates error for invalid short time' do
-    assert_has_errors 'short_time_bad_hours'
-  end
+    it 'creates error for short date time without quotes' do
+      # Unfortunately there's no way to determine this, the YAML parser
+      # returns a String regardless if a short date time is quoted or not.
+      # assert_has_errors 'short_date_time_no_quotes'
+    end
 
-  it 'creates error for invalid short date time' do
-    assert_has_errors 'short_date_time_bad_month'
-    assert_has_errors 'short_date_time_bad_hours'
-  end
-
-  it 'creates error for short date without quotes' do
-    assert_has_errors 'short_date_no_quotes'
-  end
-
-  it 'creates error for short time without quotes' do
-    assert_has_errors 'short_time_no_quotes'
-  end
-
-  it 'creates error for short date time without quotes' do
-    # Unfortunately there's no way to determine this, the YAML parser
-    # returns a String regardless if a short date time is quoted or not.
-    # assert_has_errors 'short_date_time_no_quotes'
-  end
-
-  it 'creates error if short date has wrong format' do
-    assert_has_errors 'short_date_with_wrong_short_time_format'
-    assert_has_errors 'short_date_with_wrong_short_date_time_format'
-  end
-
-  it 'creates error if short time has wrong format' do
-    assert_has_errors 'short_time_with_wrong_short_date_format'
-    assert_has_errors 'short_time_with_wrong_short_date_time_format'
-  end
-
-  it 'creates error if short date time has wrong format' do
-    assert_has_errors 'short_date_time_with_wrong_short_date_format'
-    assert_has_errors 'short_date_time_with_wrong_short_time_format'
+    it 'creates error if short date time has wrong format' do
+      assert_has_errors 'short_date_time_with_wrong_short_date_format'
+      assert_has_errors 'short_date_time_with_wrong_short_time_format'
+    end
   end
 
   private
@@ -86,5 +92,4 @@ describe Jekyll::DataValidation do
     end
     errors_count
   end
-
 end
