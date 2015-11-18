39850728a3a9cb3ca9a0653e2d3febd264c0698f
Greg Scott <i@gregoryjscott.com>
2015-11-18 11:12:24 -0700

    Restore fixtures after running date_fixer test

--- test/jekyll/data_validation/date_fixer_test.rb
+++ test/jekyll/data_validation/date_fixer_test.rb
@@ -2,7 +2,20 @@ require 'jekyll'
 require 'jekyll/data_validation'
 require 'minitest/autorun'
 
-describe Jekyll::DataValidation::Formats do
+describe Jekyll::DataValidation::DateFixer do
+
+  let(:fixtures) { File.join('test', 'fixtures') }
+  let(:saved_fixtures) { File.join('test', 'saved-fixtures') }
+
+  def setup
+    FileUtils.rm_r(saved_fixtures) if Dir.exists?(saved_fixtures)
+    FileUtils.cp_r(fixtures, saved_fixtures)
+  end
+
+  def teardown
+    FileUtils.rm_r(fixtures)
+    FileUtils.mv(saved_fixtures, fixtures)
+  end
 
   it 'does not blow up' do
     config = Jekyll.configuration({
